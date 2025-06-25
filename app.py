12927ec88b1940342d87a9b899ee97b93e3333ad
ryanlinjui <ryanlinjui@gmail.com>
2025-06-25 15:38:44 +0800

    chore: rename gemini model

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ EXAMPLE_IMAGE_LIST = [
 ]
 MODEL_LIST = [
     "Donut Model",
+    "gemini-2.5-pro",
+    "gemini-2.5-flash",
     "gemini-2.0-flash",
-    "gemini-2.5-flash-preview-05-20",
-    "gemini-2.5-pro-preview-06-05",
     "gpt-4.1",
     "gpt-4o",
     "o4-mini"
